006c73b5d6315e0b0a2c61c2d12eb2f134aaee9c
llindemann <2153696+llindemann@users.noreply.github.com>
2021-12-08 10:12:44 +0100

    Update process-openmaptiles.lua

--- resources/process-openmaptiles.lua
+++ resources/process-openmaptiles.lua
@@ -13,7 +13,7 @@ preferred_language_attribute = "name:latin"
 -- If OSM's name tag differs, then write it into this attribute (usually "name_int"):
 default_language_attribute = "name_int"
 -- Also write these languages if they differ - for example, { "de", "fr" }
-additional_languages = { }
+additional_languages = { "en", "fr", "nl" }
 --------
 
 -- Enter/exit Tilemaker
